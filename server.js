d359a46f60662ca5cc0c62d840aae1eff1ce57e0
DkeRee <dke111us@gmail.com>
2021-04-06 14:47:22 -0700

    de

--- server.js
+++ server.js
@@ -220,8 +220,8 @@ io.on('connection', socket => {
 				id: socket.id,
 				username: username.trim(),
 				coords: {
-					x: Math.floor((Math.random() * 300) + -300),
-					y: Math.floor((Math.random() * 300) + -300)
+					x: Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1),
+					y: Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1)
 				},
 				color: colors[Math.floor(Math.random() * colors.length)],
 				time: 60000,
